d40588d0e059bc141a838e7a42a806a282a805a5
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-09-17 22:57:52 +0300

    a

--- src/lib/roblox.ts
+++ src/lib/roblox.ts
@@ -1,5 +1,6 @@
 import { getIdFromUsername, getPlayerInfo, PlayerInfo } from "noblox.js";
 import { UserIdResolveCacheWipeInterval } from "./Constants";
+import { logger } from "./Utility";
 
 let usernameIdCache: {[username: string]: number} = {};
 let playerInfoCache: {[userid: string]: PlayerInfo} = {};
@@ -22,9 +23,14 @@ export async function GetUserDetails(userid: number): Promise<PlayerInfo> {
 	if (playerInfoCache[userid]) {
 		return playerInfoCache[userid];
 	}
-	const d = await await getPlayerInfo(userid);
-	playerInfoCache[userid] = d;
-	return d;
+	try {
+		const d = await getPlayerInfo(userid);
+		playerInfoCache[userid] = d;
+		return d;
+	} catch(e_) {
+		logger.child({error:e_}).info("Shitty code, Error!");
+	};
+	return playerInfoCache[userid] || {} as PlayerInfo;
 }
 
 // someone can switch their roblox username with someone elses, fucking up this caching thing or whatever the fuck this monstrosity is
