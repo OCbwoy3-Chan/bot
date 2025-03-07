ae82262211ad11e3baf755dc777f747902238dc8
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-07 15:30:06 +0200

    d

--- src/services/Server/router/stats.ts
+++ src/services/Server/router/stats.ts
@@ -1,9 +1,36 @@
-import { Presence } from "discord.js";
+import { Activity, Presence } from "discord.js";
 import express from "express";
 
 export const router = express.Router();
 
-let presence: Presence | null = null;
+let presence: Presence | null = {
+	userId: "486147449703104523",
+	guild: "1262483043654832259" as any,
+	status: "dnd",
+	activities: [
+		{
+			name: "OCbwoy3-Chan Music",
+			type: 2,
+			url: null,
+			details: "BROKEN STATUS",
+			state: "@ocbwoy3 fix!!!",
+			applicationId: "886578863147192350",
+			timestamps: {},
+			party: null,
+			syncId: null,
+			assets: {},
+			flags: 0,
+			emoji: null,
+			buttons: [
+				"TEST"
+			],
+			createdTimestamp: 1741353638376
+		}
+	] as any as Activity[],
+	clientStatus: {
+		embedded: "dnd"
+	} as any
+} as any
 
 export function setPresence(p: any | null) {
 	presence = p;
