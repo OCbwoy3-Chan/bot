cfc69befa6fa4eae322f2061a82e74ac7f771e7c
dev0xz02 <dev0xz02@duck.com>
2025-03-09 08:40:32 +0000

    Russian language for i18n/l10n

--- src/services/Bot/languages/l10n_core.ts
+++ src/services/Bot/languages/l10n_core.ts
@@ -2,7 +2,8 @@ import { resolveKey } from "@sapphire/plugin-i18next";
 
 export const ALL_LANGUAGES: { id: string, name: string, english: string }[] = [
 	{ id: "en", name: "English", english: "English" },
-	{ id: "lv", name: "Latviešu", english: "Latvian" }
+	{ id: "lv", name: "Latviešu", english: "Latvian" },
+	{ id: "ru", name: "Русский", english: "Russian" }
 ]
 
 export const r = resolveKey;
