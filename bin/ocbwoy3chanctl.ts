14c90488f68506641ed3f15f53ba23972d03a335
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-03-11 19:36:55 +0200

    a

--- bin/ocbwoy3chanctl.ts
+++ bin/ocbwoy3chanctl.ts
@@ -23,26 +23,28 @@ if (opts.update) {
 		throw "path not exists";
 	}
 
-	$`cd ${PATH} && git stash`.nothrow().quiet();
+	(async()=>{
+		await $`cd ${PATH} && git stash`.nothrow().quiet();
 
-	console.log(chalk.cyanBright("Pulling latest changes"))
-	$`cd ${PATH} && git pull --rebase`.nothrow();
-	$`cd ${PATH} && git stash drop`.nothrow().quiet();
+		console.log(chalk.cyanBright("Pulling latest changes"))
+		await $`cd ${PATH} && git pull --rebase`.nothrow();
+		await $`cd ${PATH} && git stash drop`.nothrow().quiet();
 
-	console.log(chalk.cyanBright("Installing new packages"))
-	$`cd ${PATH} && bun i`.nothrow();
+		console.log(chalk.cyanBright("Installing new packages"))
+		await $`cd ${PATH} && bun i`.nothrow();
 
-	console.log(chalk.cyanBright("Compiling ocbwoy3chanctl"))
-	$`cd ${PATH} && bun build --compile --minify --outfile dist/ocbwoy3chanctl bin/ocbwoy3chanctl.ts`.nothrow();
+		console.log(chalk.cyanBright("Compiling ocbwoy3chanctl"))
+		await $`cd ${PATH} && bun build --compile --minify --outfile dist/ocbwoy3chanctl bin/ocbwoy3chanctl.ts`.nothrow();
 
-	console.log(chalk.cyanBright("Copying ocbwoy3chanctl to usr bin"))
-	$`echo ${PASSWORD} | -S rm /usr/bin/ocbwoy3chanctl`.nothrow().quiet();
-	$`echo ${PASSWORD} | -S --chdir=${PATH} cp dist/ocbwoy3chanctl /usr/bin/`.nothrow().quiet();
+		console.log(chalk.cyanBright("Copying ocbwoy3chanctl to usr bin"))
+		await $`echo ${PASSWORD} | -S rm /usr/bin/ocbwoy3chanctl`.nothrow().quiet();
+		await $`echo ${PASSWORD} | -S --chdir=${PATH} cp dist/ocbwoy3chanctl /usr/bin/`.nothrow().quiet();
 
 
-	console.log(chalk.cyanBright("Migrating db"))
-	$`cd ${PATH} && bunx prisma migrate`.nothrow();
+		console.log(chalk.cyanBright("Migrating db"))
+		await $`cd ${PATH} && bunx prisma migrate`.nothrow();
 
-	console.log(chalk.cyanBright("Restaring 112"))
-	$`pm2 restart 112`.nothrow();
+		console.log(chalk.cyanBright("Restaring 112"))
+		await $`pm2 restart 112`.nothrow();
+	})()
 }
