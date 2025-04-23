dae8db0a79e36e29423e6f985a886983b09d4e3d
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-04-23 09:15:47 +0300

    d

--- src/services/Database/db.ts
+++ src/services/Database/db.ts
@@ -1,3 +1,15 @@
+import { logger } from "@112/Utility";
 import { PrismaClient } from "@prisma/client";
+import { platform } from "os";
 
-export const prisma = new PrismaClient();
+function _createPrisma(): PrismaClient {
+	const st = performance.now();
+	let c = new PrismaClient();
+	const stt = performance.now();
+	if ((stt-st) > 250) {
+		logger.warn(`DB took ${Math.ceil(stt-st)}ms to start.${platform() === "win32" ? " This is YOUR fault for using W*ndows." : ""}`)
+	}
+	return c
+}
+
+export const prisma = _createPrisma();
