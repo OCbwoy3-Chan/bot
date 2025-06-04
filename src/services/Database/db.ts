320783a8fa4e934d8df2dc8d247c6e8b52805250
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-06-04 23:21:03 +0300

    this is faulty

--- src/services/Database/db.ts
+++ src/services/Database/db.ts
@@ -6,11 +6,9 @@ function _createPrisma(): PrismaClient {
 	const st = performance.now();
 	let c = new PrismaClient();
 	const stt = performance.now();
-	if (stt - st > 250) {
-		logger.warn(
-			`DB took ${Math.ceil(stt - st)}ms to start.${platform() === "win32" ? " This is YOUR fault for using W*ndows." : ""}`
-		);
-	}
+	logger.warn(
+		`DB took ${Math.ceil(stt - st)}ms to start.${platform() === "win32" ? " This is YOUR fault for using Windows!!!!!!!" : ""}`
+	);
 	return c;
 }
 
